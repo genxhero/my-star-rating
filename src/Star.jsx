05a6517f1552d9e8445bb44e0a3818dc33a44439
genxhero <genxhero@users.noreply.github.com>
2020-03-09 22:58:06 -0700

    comments for star

--- src/Star.jsx
+++ src/Star.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import {MdStar, MdStarBorder, MdStarHalf} from 'react-icons/md';
 import PropTypes from 'prop-types';
 
-
+/**
+ * Clickable star. Should be a filled star when rating is equal to or greater than star's position.
+ * @param {*} props 
+ */
 const Star = props => {
 
     Star.propTypes = {
